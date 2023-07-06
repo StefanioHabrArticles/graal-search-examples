9eecfc0a9ec9c6225e00bdd3358ef033a5637ca9
StefanioHabrArticles <diggerss13@yandex.ru>
2023-07-06 23:49:00 +0300

    structure changed

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,6 +5,23 @@
         // ReSharper disable once UnusedParameter.Local
         private static void Main(string[] args)
         {
+            Example1();
+
+            Example2();
+
+            Example3();
+            
+            Example4();
+            
+            Example5();
+
+            Example6();
+
+            Example7();
+        }
+
+        private static void Example1()
+        {
             var people = new List<Person>
             {
                 Person.AdditiveIdentity,
@@ -14,7 +31,10 @@
             };
             var richest = people.Sum();
             Console.WriteLine(richest.Name);
+        }
 
+        private static void Example2()
+        {
             var digitOrLetter = new List<Predicate<char>>
                 {
                     x => x is >= '0' and <= '9',
@@ -24,7 +44,10 @@
                 .Select(x => (Any<char>)x)
                 .ToList().Sum();
             Console.WriteLine($"$ - {digitOrLetter.Test('$')}; Z - {digitOrLetter.Test('Z')}; 5 - {digitOrLetter.Test('5')}");
+        }
 
+        private static void Example3()
+        {
             var stringsCatalogue = new Catalogue<string>()
                 .Add("cabcc")
                 .Pass()
@@ -32,35 +55,53 @@
                 .Remove("a")
                 .Add("ed");
             Console.WriteLine(stringsCatalogue.Reduce(s => s.Length));
+
             var chars = stringsCatalogue.Reduce<PairedEnumerable<List<char>, ListWrapper<char>, char>>(
                 s => s.ToList()
             ).Merge();
             Console.WriteLine(string.Join("", chars));
+        }
 
+        private static void Example4()
+        {
             var someDtoCatalogue = new Catalogue<SomeDto>
             {
-                new SomeDto(1, false, "asa"),
-                new SomeDto(2, true, "asa")
-            }.Remove(new SomeDto(1, false, "asa"));
+                new SomeDto(1, false, "abc"),
+                new SomeDto(2, true, "def"),
+                new SomeDto(3, true, "ghi")
+            }.Remove(new SomeDto(1, false, "abc"));
+            
             Console.WriteLine(someDtoCatalogue.Reduce(_ => 1));
+            Console.WriteLine(someDtoCatalogue.Reduce(x => new AveragedValue(x.Number)).Get());
+
             var logs = someDtoCatalogue.ToList();
             logs.ForEach(Console.WriteLine);
+
             var someDtoObjects = someDtoCatalogue.Collect();
             someDtoObjects.ForEach(Console.WriteLine);
+        }
 
+        private static void Example5()
+        {
             var filter = new Catalogue<Predicate<int>>()
                 .Add(x => x > 2)
                 .Remove(x => x > 5)
                 .Reduce(x => (All<int>)x);
             Console.WriteLine($"4 - {filter.Test(4)}; 6 - {filter.Test(6)}");
-
+        }
+        
+        private static void Example6()
+        {
             SquareMatrix<int> testMatrix = new(3,
                 new[] { 1, 0, 3 },
                 new[] { 0, 5, 0 },
                 new[] { 2, 0, 6 }
             );
             Console.WriteLine(testMatrix ^ 4);
+        }
 
+        private static void Example7()
+        {
             var weightedGraph = new WeightedGraph(4,
                 (0, new() { (1, 3), (2, 8), (3, 12) }),
                 (1, new() { (2, 2), (3, 10) }),
