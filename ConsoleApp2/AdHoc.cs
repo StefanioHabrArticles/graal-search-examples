a12f570ff940d14d97f7ddffcbabea497e394fb8
StefanioHabrArticles <diggerss13@yandex.ru>
2023-08-26 21:56:46 +0300

    adhoc 2

--- ConsoleApp2/AdHoc.cs
+++ ConsoleApp2/AdHoc.cs
@@ -5,6 +5,9 @@ class Appender
     public T AppendItems<TAppendable, T>(T a, T b)
         where TAppendable : struct, IAppendable<T> =>
         default(TAppendable).Append(a, b);
+
+    public T AppendItems<T>(AppendableValue<T> a, AppendableValue<T> b) =>
+        a.Append(b);
 }
 
 interface IAppendable<T>
@@ -24,11 +27,43 @@ struct AppendableString : IAppendable<string>
         $"{a}{b}";
 }
 
+abstract class AppendableValue<T>
+{
+    public T Value { get; }
+
+    protected AppendableValue(T value) =>
+        Value = value;
+
+    public abstract T Append(AppendableValue<T> item);
+}
+
+class AppendableIntValue :
+    AppendableValue<int>
+{
+    public AppendableIntValue(int value) :
+        base(value) { }
+
+    public override int Append(AppendableValue<int> item) =>
+        Value + item.Value;
+}
+
+class AppendableStringValue :
+    AppendableValue<string>
+{
+    public AppendableStringValue(string value) :
+        base(value) { }
+
+    public override string Append(AppendableValue<string> item) =>
+        $"{Value}{item.Value}";
+}
+
 class Example
 {
     public void Test()
     {
         Console.WriteLine(new Appender().AppendItems<AppendableInt, int>(1, 2));
         Console.WriteLine(new Appender().AppendItems<AppendableString, string>("1", "2"));
+
+        Console.WriteLine(new Appender().AppendItems(new AppendableIntValue(1), new AppendableIntValue(2)));
     }
 }
